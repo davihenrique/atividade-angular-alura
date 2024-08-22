d520456fa16e6477198cd3446a2efbc4d824832f
Davi Silva <davi.silva2597@gmail.com>
2024-08-22 17:46:14 -0300

    add: project

---
+++ src/app/componentes/pensamentos/pensamento.service.ts
@@ -0,0 +1,35 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Pensamento } from './pensamento';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class PensamentoService {
+  private readonly API = ' http://localhost:3000/pensamentos';
+  constructor(private http: HttpClient) {}
+
+  listar(): Observable<Pensamento[]> {
+    return this.http.get<Pensamento[]>(this.API);
+  }
+
+  criar(p: Pensamento): Observable<Pensamento> {
+    return this.http.post<Pensamento>(this.API, p);
+  }
+
+  editar(p: Pensamento): Observable<Pensamento> {
+    const url = `${this.API}/${p.id}`;
+    return this.http.put<Pensamento>(url, p);
+  }
+
+  excluir(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`;
+    return this.http.delete<Pensamento>(url);
+  }
+
+  buscarPorId(id: number): Observable<Pensamento> {
+    const url = `${this.API}/${id}`;
+    return this.http.get<Pensamento>(url);
+  }
+}
