aef68bb0dff8419d26aff9b17cc98bb486ac4015
Davi Henrique dos Santos Silva <davi.silva2597@gmail.com>
2025-01-01 20:25:32 -0300

    aula 5

--- src/app/service/lista-de-compra.service.ts
+++ src/app/service/lista-de-compra.service.ts
@@ -30,7 +30,7 @@ export class ListaDeCompraService {
   adicionarItemNaLista(nomeDoItem: string) {
     const item = this.criarItem(nomeDoItem);
     this.listaDeCompra.push(item);
-    // this.atualizarLocalStorage();
+    this.atualizarLocalStorage();
   }
 
   editarItemDaLista(itemAntigo: Item, nomeEditadoDoItem: string) {
@@ -43,7 +43,12 @@ export class ListaDeCompraService {
     const id = itemAntigo.id;
 
     this.listaDeCompra.splice(Number(id) - 1, 1, itemEditado);
-    // this.atualizarLocalStorage();
+    this.atualizarLocalStorage();
+  }
+
+  removerTodos() {
+    this.listaDeCompra = [];
+    this.atualizarLocalStorage();
   }
 
   atualizarLocalStorage() {
