e4c5f951ee93fa1afc01533f7e084bf750be134c
Davi Henrique dos Santos Silva <davi.silva2597@gmail.com>
2025-01-01 18:44:19 -0300

    aula 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,16 +5,20 @@ import { ListaDeCompraService } from './service/lista-de-compra.service';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent implements OnInit {
   title = 'app-lista-de-compras';
-  listaDeCompra! : Array<Item>
-  constructor(private listaService: ListaDeCompraService) { }
+  listaDeCompra!: Array<Item>;
+  itemParaSerEditado!: Item;
+  constructor(private listaService: ListaDeCompraService) {}
 
   ngOnInit(): void {
     this.listaDeCompra = this.listaService.getListaDeCompra();
     console.log(this.listaDeCompra);
   }
 
+  editarItem(item: Item) {
+    this.itemParaSerEditado = item;
+  }
 }
--- src/app/components/item/item.component.ts
+++ src/app/components/item/item.component.ts
@@ -1,25 +1,38 @@
-import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  Input,
+  OnChanges,
+  OnInit,
+  Output,
+  SimpleChanges,
+} from '@angular/core';
 import { faPen, faTrash } from '@fortawesome/free-solid-svg-icons';
 import { Item } from 'src/app/interfaces/iItem';
 
 @Component({
   selector: 'app-item',
   templateUrl: './item.component.html',
-  styleUrls: ['./item.component.css']
+  styleUrls: ['./item.component.css'],
 })
 export class ItemComponent implements OnInit, OnChanges {
   @Input() item!: Item;
+  @Output() emitindoItemParaEditar = new EventEmitter();
 
   faPen = faPen;
-  faTrash = faTrash
+  faTrash = faTrash;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
     console.log('OnInit');
-   }
+  }
 
   ngOnChanges(changes: SimpleChanges): void {
     console.log('OnChanges');
+  }
+
+  editarItem() {
+    this.emitindoItemParaEditar.emit(this.item);
   }
 }
