df729b91f438b88952081c7f8fe399422ee7b05d
Davi Silva <davi.henrique.silva597@gmail.com>
2025-03-03 21:01:31 -0300

    finalizando projeto

--- Explore o Framework Angular/01 - Angular 14: aplique os conceitos e desenvolva seu primeiro CRUD/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ Explore o Framework Angular/01 - Angular 14: aplique os conceitos e desenvolva seu primeiro CRUD/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Pensamento } from '../pensamento';
+import { PensamentoService } from '../pensamento.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-criar-pensamento',
@@ -7,22 +10,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CriarPensamentoComponent implements OnInit {
 
-  pensamento = {
-    id: '1',
-    conteudo: 'Aprendendo Angular',
-    autoria: 'Dev',
-    modelo: 'modelo1',
+  pensamento: Pensamento = {
+    conteudo: '',
+    autoria: '',
+    modelo: '',
   };
 
-  constructor() {}
+  constructor(
+    private service: PensamentoService,
+  private router: Router) {}
 
   ngOnInit(): void {}
 
   criarPensamento() {
-    alert("Pensamento criado!");
+    this.service.criar(this.pensamento).subscribe(()=>{
+      this.router.navigate(['listarPensamento']);
+    });
   }
 
   cancelar() {
-    alert("Operação cancelada!");
+    this.router.navigate(['listarPensamento']);
   }
 }
