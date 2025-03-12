c9902017f95df7abdf1e6111715878128adb13be
Davi Henrique dos Santos Silva <davi.silva2597@gmail.com>
2025-03-12 20:40:47 -0300

    aula 2

--- Explore o Framework Angular/02 - Angular 14: evoluindo a aplicação/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
+++ Explore o Framework Angular/02 - Angular 14: evoluindo a aplicação/memoteca/src/app/componentes/pensamentos/criar-pensamento/criar-pensamento.component.ts
@@ -20,26 +20,36 @@ export class CriarPensamentoComponent implements OnInit {
 
   ngOnInit(): void {
     this.formulario = this.formBuild.group({
-      conteudo: ['', [Validators.required]],
-      autoria: ['',[Validators.required]],
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/), 
+      ])],
+      autoria: ['',Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+      ])],
       modelo: ['modelo1'],
     })
   }
 
-
-
   criarPensamento() {
-
-    console.log(this.formulario);
+    console.log(this.formulario.get('autoria')?.errors);
     if (this.formulario.valid) {
       this.service.criar(this.formulario.value).subscribe(()=>{
         this.router.navigate(['listarPensamento']);
       });
     }
-
   }
 
   cancelar() {
     this.router.navigate(['listarPensamento']);
+  }
+
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return 'botao';
+    }else{
+      return 'botao__desabilitado';
+    }
   }
 }
--- Explore o Framework Angular/02 - Angular 14: evoluindo a aplicação/memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
+++ Explore o Framework Angular/02 - Angular 14: evoluindo a aplicação/memoteca/src/app/componentes/pensamentos/editar-pensamento/editar-pensamento.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Pensamento } from '../pensamento';
 import { PensamentoService } from '../pensamento.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-editar-pensamento',
@@ -16,17 +17,32 @@ export class EditarPensamentoComponent implements OnInit {
   autoria:'',
   modelo:''
   }
+  
+  formulario!: FormGroup;
 
   constructor(
     private service: PensamentoService,
     private router: Router,
-    private route: ActivatedRoute
+    private route: ActivatedRoute,
+    private formBuild: FormBuilder
   ) { }
 
   ngOnInit(): void {
     const id = this.route.snapshot.paramMap.get('id');
     this.service.buscarPorId(parseInt(id!)).subscribe((pensamento) => {
       this.pensamento = pensamento;
+    })
+
+    this.formulario = this.formBuild.group({
+      conteudo: ['', Validators.compose([
+        Validators.required,
+        Validators.pattern(/(.|\s)*\S(.|\s)*/), 
+      ])],
+      autoria: ['',Validators.compose([
+        Validators.required,
+        Validators.minLength(3),
+      ])],
+      modelo: ['modelo1'],
     })
   }
 
@@ -35,11 +51,17 @@ export class EditarPensamentoComponent implements OnInit {
       this.router.navigate(['listarPensamento']);
     }
     );
-
   }
 
   cancelar(){
     this.router.navigate(['listarPensamento']);
   }
 
+  habilitarBotao(): string {
+    if(this.formulario.valid) {
+      return 'botao';
+    }else{
+      return 'botao__desabilitado';
+    }
+  }
 }
