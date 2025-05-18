153d2d28b6ea8b789e30ade06969e698a72c9497
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2025-05-18 15:30:36 -0300

    add tarefasFiltradas

--- Animacoes/src/app/animations.ts
+++ Animacoes/src/app/animations.ts
@@ -1,4 +1,4 @@
-import { animate, state, style, transition, trigger } from "@angular/animations";
+import { animate, keyframes, state, style, transition, trigger } from "@angular/animations";
 
 export const highlightedStateTrigger = trigger('highlightedState',[
     state('default',style({
@@ -39,4 +39,17 @@ export const highlightedStateTrigger = trigger('highlightedState',[
         }))
     ])
   ])
-  
+
+  export const filterTrigger = trigger('filterAnimation',[
+    transition(':enter',[
+        style({opacity: 0, width: 0}),
+        animate('400ms ease-out', keyframes([
+            style({offset: 0, opacity:0, width:0}),
+            style({offset: 0.8, opacity:0.5, width:'*'}),
+            style({offset: 1, opacity:1, width:'*'}),
+        ]))
+    ]),
+    transition(':leave',[
+        animate('400ms cubic-bezier(.13,.9,.8,.1)', style({opacity:0, width: 0}))
+    ])
+  ])
--- Animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
+++ Animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,12 +4,16 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { checkButtonTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
+import { checkButtonTrigger, filterTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger, shownStateTrigger, checkButtonTrigger]
+  animations: [
+    highlightedStateTrigger, 
+    shownStateTrigger, 
+    checkButtonTrigger,
+    filterTrigger]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
@@ -18,6 +22,8 @@ export class ListaTarefasComponent implements OnInit {
   validado: boolean = false;
   indexTarefa: number = -1;
   id: number = 0;
+  campoBusca: string = '';
+  tarefasFiltradas: Tarefa[] = [];
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -36,8 +42,20 @@ export class ListaTarefasComponent implements OnInit {
   ngOnInit(): Tarefa[] {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
       this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
     });
-    return this.listaTarefas;
+    return this.tarefasFiltradas;
+  }
+
+  filtrarTarefasPorDescricao(descricao: string){
+    this.campoBusca = descricao.trim().toLowerCase();
+
+    if(descricao){
+      this.tarefasFiltradas = this.listaTarefas.filter(terefa =>
+        terefa.descricao.toLowerCase().includes(this.campoBusca))
+      } else {
+        this.tarefasFiltradas = this.listaTarefas
+      }
   }
 
   mostrarOuEsconderFormulario() {
@@ -120,7 +138,7 @@ export class ListaTarefasComponent implements OnInit {
 
   listarAposCheck() {
     this.service.listar(this.categoria).subscribe((listaTarefas) => {
-      this.listaTarefas = listaTarefas;
+      this.tarefasFiltradas = listaTarefas;
     });
   }
 
