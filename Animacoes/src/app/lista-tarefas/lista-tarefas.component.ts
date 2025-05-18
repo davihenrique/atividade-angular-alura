f30b5c00513884faf0ded71f2155dd9f9adeb789
Davi Henrique dos Santos Silva <davi.silva2597@gmail.com>
2025-05-18 14:00:34 -0300

    aula 05

--- Animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
+++ Animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,19 +4,20 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { highlightedStateTrigger } from '../animations';
+import { checkButtonTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
   styleUrls: ['./lista-tarefas.component.css'],
-  animations: [highlightedStateTrigger]
+  animations: [highlightedStateTrigger, shownStateTrigger, checkButtonTrigger]
 })
 export class ListaTarefasComponent implements OnInit {
   listaTarefas: Tarefa[] = [];
   formAberto: boolean = false;
   categoria: string = '';
   validado: boolean = false;
-  indexTarefa = -1;
+  indexTarefa: number = -1;
+  id: number = 0;
 
   formulario: FormGroup = this.fomBuilder.group({
     id: [0],
@@ -109,6 +110,7 @@ export class ListaTarefasComponent implements OnInit {
   }
 
   finalizarTarefa(id: number) {
+    this.id = id;
     this.service.buscarPorId(id!).subscribe((tarefa) => {
       this.service.atualizarStatusTarefa(tarefa).subscribe(() => {
         this.listarAposCheck();
