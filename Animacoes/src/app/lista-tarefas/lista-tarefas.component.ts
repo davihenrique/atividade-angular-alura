3370768174b34dd6d8e6662d12233218758e958d
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2025-06-15 17:26:50 -0300

    add aula 03

--- Animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
+++ Animacoes/src/app/lista-tarefas/lista-tarefas.component.ts
@@ -4,7 +4,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 import { TarefaService } from 'src/app/service/tarefa.service';
 import { Tarefa } from '../interface/tarefa';
-import { checkButtonTrigger, filterTrigger, formButtonTrigger, highlightedStateTrigger, shownStateTrigger } from '../animations';
+import { checkButtonTrigger, filterTrigger, formButtonTrigger, highlightedStateTrigger, sahakeTrigger, shownStateTrigger } from '../animations';
 @Component({
   selector: 'app-lista-tarefas',
   templateUrl: './lista-tarefas.component.html',
@@ -14,7 +14,8 @@ import { checkButtonTrigger, filterTrigger, formButtonTrigger, highlightedStateT
     shownStateTrigger, 
     checkButtonTrigger,
     filterTrigger,
-    formButtonTrigger
+    formButtonTrigger,
+    sahakeTrigger
   ]
 })
 export class ListaTarefasComponent implements OnInit {
