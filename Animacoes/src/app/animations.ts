1786994b971fe2830864391d4bd4d4e786f7d96a
Davi Henrique dos Santos Silva <davi.henrique.silva@outlook.com>
2025-06-15 22:49:04 -0300

    add: Aula 05

--- Animacoes/src/app/animations.ts
+++ Animacoes/src/app/animations.ts
@@ -3,6 +3,7 @@ import {
   group,
   keyframes,
   query,
+  stagger,
   state,
   style,
   transition,
@@ -153,5 +154,30 @@ export const shakeTrigger = trigger('shakeAnimation',[
         style({ transform: 'translateX(0)'}),
       ]))
     ],{optional: true})
+  ])
+])
+
+export const listStateTrigger = trigger('listState', [
+  transition('* => *', [
+    query(':enter', [
+      style({
+        opacity: 0,
+        transform: 'translateX(-100%)',
+      }),
+      stagger(200, [
+        animate('500ms ease-out', keyframes([
+          style({
+            opacity: 1,
+            transform: 'translateX(15%)',
+            offset: 0.4
+          }),
+          style({
+            opacity: 1,
+            transform: 'translateX(0)',
+            offset: 1
+          })
+        ]))
+      ])
+    ], {optional: true})
   ])
 ])
